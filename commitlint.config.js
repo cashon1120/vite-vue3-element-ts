8223531806a5048ca0987f367faaea4d84bc97c9
cashon1120 <49798613@qq.com>
2022-07-20 14:00:51 +0800

    chore: 添加husky钩子

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,23 @@
-module.exports = {extends: ['@commitlint/config-conventional']}
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-case": [2, "always", ["lower-case", "upper-case"]],
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat", // 新特性，新功能
+        "fix", // 修改bug
+        "docs", // 文档修改
+        "style", // 代码格式修改, 注意不是 css 修改
+        "refactor", // 代码重构
+        "perf", // 优化相关，比如提升性能、体验
+        "test", // 测试用例修改
+        "chore", // 其他修改, 比如改变构建流程、或者增加依赖库、工具等
+        "revert", // 回滚到上一个版本
+        "ci", // 持续集成修改
+        "build", // 编译相关的修改，例如发布版本、对项目构建或者依赖的改动
+      ],
+    ],
+  },
+};
